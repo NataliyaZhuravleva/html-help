d5132ae4404adc970581aa662d4be6276d525426
Zahnen Garner <zahnen@gmail.com>
2020-10-20 09:44:08 -0700

    Add jQuery functionality to walrus img
    Co-authored-by: Nataliya Zhuravleva<natalindria@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
-jQuery(document).ready(function() {
- jQuery("h1").click(function() {
-    alert("This is a header.");
+/*$(document).ready(function() {
+  $(".clickable").click(function() {
+    $("#walrus-showing").toggle();
+    $("#walrus-hidden").toggle();
   });
-  jQuery("p").click(function() {
-    alert("This is a paragraph.");
-  });
-  jQuery("img").click(function() {
-    alert("This is an image.");
-    alert("I told you, THIS IS A HEADER!");
+});
+*/$(document).ready(function() {
+  $(".clickable").click(function() {
+    $("#walrus-showing").slideToggle();
+    $("#walrus-hidden").slideToggle();
   });
 });
